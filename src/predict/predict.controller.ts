76e45649d47b4ef9f93b9d17556c3a1b187d1d34
adam20092001 <adam20092001@gmail.com>
2025-06-20 14:51:26 -0500

    actualizacion controlle 2 y main version 2

--- src/predict/predict.controller.ts
+++ src/predict/predict.controller.ts
@@ -11,24 +11,39 @@ import { Express } from 'express';
 import { extname } from 'path';
 import * as AWS from 'aws-sdk';
 import * as multerS3 from 'multer-s3';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const region = process.env.AWS_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const bucket = process.env.AWS_S3_BUCKET;
+
+if (!region || !accessKeyId || !secretAccessKey || !bucket) {
+  throw new Error('❌ Faltan variables de entorno AWS.');
+}
+
 const s3 = new AWS.S3({
-  region: process.env.AWS_REGION,
+  region,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId,
+    secretAccessKey,
   },
 });
 
+
 const storage = multerS3({
   s3,
-  bucket: process.env.AWS_S3_BUCKET!,
+  bucket,
   acl: 'public-read',
   contentType: multerS3.AUTO_CONTENT_TYPE,
   key: (req, file, cb) => {
-    const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1e9) + extname(file.originalname);
+    const uniqueName =
+      Date.now() + '-' + Math.round(Math.random() * 1e9) + extname(file.originalname);
     cb(null, uniqueName);
   },
 });
+
 interface S3MulterFile extends Express.Multer.File {
   key: string;
   location: string;
