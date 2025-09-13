54bad963c6fb42c578c35b80b743a6cb44fe2a21
adam20092001 <adam20092001@gmail.com>
2025-09-12 22:53:36 -0500

    creacion entidad notification 3

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Notification } from './entities/notification.entity';
+
 
 @Module({
   imports: [TypeOrmModule.forFeature([Notification])],
