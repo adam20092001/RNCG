0e59f3cad5c206057e87efb433f3c01e7fafcfcb
adam20092001 <adam20092001@gmail.com>
2025-09-12 23:29:53 -0500

    implementacion de notificaciones en prediccion >80% v2

--- src/predictions/predictions.module.ts
+++ src/predictions/predictions.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Prediction } from './entities/prediction.entity';
 import { User } from 'src/users/entities/user.entity';
 import { Patient } from 'src/patients/entities/patient.entity';
+import { NotificationsModule } from 'src/notifications/notifications.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Prediction, User, Patient])],
+  imports: [TypeOrmModule.forFeature([Prediction, User, Patient]),NotificationsModule],
   providers: [PredictionsService],
   controllers: [PredictionsController],
   exports: [PredictionsService]
